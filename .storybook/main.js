5b5263080a60f85ef7a22d67233705c66c9c5892
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2026-03-30 17:34:53 +0200

    Initial project setup - 30-03-2026

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+
+
+/** @type { import('@storybook/sveltekit').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|ts|svelte)",
+  ],
+  "addons": [
+    "@storybook/addon-svelte-csf",
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+  ],
+  "framework": "@storybook/sveltekit",
+  "core": {
+    "disableTelemetry": true,
+  },
+};
+export default config;
