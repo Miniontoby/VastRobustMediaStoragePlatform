e9397a59f4ef0550401766ab35b0bf01552e2471
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2026-04-04 20:28:08 +0200

    Update build.yml to fix indents (#1)
    * Update build.yml to fix indents
    * Added missing lock file
    * Fix issues with auth being loaded during build
    * Update workflow to update the step dependencies from old project to match latest versions

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -1,3 +1,4 @@
+import { building } from '$app/environment';
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
@@ -5,6 +6,6 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = mysql.createPool(env.DATABASE_URL);
+const client = !building ? mysql.createPool(env.DATABASE_URL) : null;
 
-export const db = drizzle(client, { schema, mode: 'default' });
+export const db = !building ? drizzle(client, { schema, mode: 'default' }) : null;
