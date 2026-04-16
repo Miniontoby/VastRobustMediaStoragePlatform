60c298c608156fe653748d562576c05c37bbc07c
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2026-04-16 08:39:52 +0200

    Feat: Add styling to upload page

--- src/routes/(uploader)/upload/+page.svelte
+++ src/routes/(uploader)/upload/+page.svelte
@@ -21,7 +21,7 @@
 			const file = files[0];
 			try {
 				const response = await uploadFile(file, updateProgressCallback);
-				console.log(response)
+				console.log(response);
 				progress = 100;
 				// TODO Add message when done
 			} catch (e) {
@@ -45,25 +45,42 @@
 
 <h1>{m['pages.upload.title']()}</h1>
 <p>{m['pages.upload.description']()}</p>
+<br>
 
-<form onsubmit={submitCallback}>
+<form onsubmit={submitCallback} method="post">
 	<input type="file" id="video" name="video" bind:files={files} accept="video/*" /><br/>
-	<button disabled={progress !== null}>{m['actions.upload']()}</button>
+	<button class="btn btn-primary" disabled={progress !== null}>{m['actions.upload']()}</button>
 </form>
 
 {#if progress !== null}
-	<div role="alert" class="popup">
-		{#if progress === 100}
-			<h1>{m['pages.upload.uploaded.title']()}</h1>
-			<br>
-			<p>{m['pages.upload.uploaded.description']()}</p>
-			<button onclick={closePopup}>{m['actions.okay']()}</button>
-		{:else}
-			<h1>{m['pages.upload.uploading.title']()}</h1>
-			<br>
-			<p>{m['pages.upload.uploading.description']({ progress })}</p>
-			<button onclick={cancelUpload}>{m['actions.cancel']()}</button>
-		{/if}
-	</div>
+	<dialog id="popup-modal" role="alert" tabindex="-1" aria-labelledby="dialog-title" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+		<div class="relative p-4 w-full max-w-md max-h-full">
+			<div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
+				<button type="button" class="absolute top-3 end-2.5 text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" onclick={closePopup}>
+					<svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
+					<span class="sr-only">Close modal</span>
+				</button>
+				<div class="p-4 md:p-5 text-center">
+					{#if progress === 100}
+						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploaded.title']()}</h2>
+						<div class="mt-2">
+							<p class="text-sm text-gray-400">{m['pages.upload.uploaded.description']()}</p>
+						</div>
+						<div class="flex items-center space-x-4 justify-center">
+							<button type="button" class="btn btn-success" onclick={closePopup}>{m['actions.okay']()}</button>
+						</div>
+					{:else}
+						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploading.title']()}</h2>
+						<div class="mt-2">
+							<p class="text-sm text-gray-400">{m['pages.upload.uploading.description']()}</p>
+						</div>
+						<div class="flex items-center space-x-4 justify-center">
+							<button type="button" class="btn btn-danger" onclick={cancelUpload}>{m['actions.cancel']()}</button>
+						</div>
+					{/if}
+				</div>
+			</div>
+		</div>
+	</dialog>
 {/if}
 
