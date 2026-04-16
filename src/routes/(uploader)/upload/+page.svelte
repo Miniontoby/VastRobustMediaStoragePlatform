69d98acd630736b463a910951e28ea2da34256bd
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2026-04-16 00:57:42 -0600

    Hotfix: fix stylings

--- src/routes/(uploader)/upload/+page.svelte
+++ src/routes/(uploader)/upload/+page.svelte
@@ -53,7 +53,7 @@
 </form>
 
 {#if progress !== null}
-	<dialog id="popup-modal" role="alert" tabindex="-1" aria-labelledby="dialog-title" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+	<dialog id="popup-modal" tabindex="-1" aria-labelledby="dialog-title" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex">
 		<div class="relative p-4 w-full max-w-md max-h-full">
 			<div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
 				<button type="button" class="absolute top-3 end-2.5 text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" onclick={closePopup}>
@@ -62,7 +62,7 @@
 				</button>
 				<div class="p-4 md:p-5 text-center">
 					{#if progress === 100}
-						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploaded.title']()}</h2>
+						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploaded.title']()}</h3>
 						<div class="mt-2">
 							<p class="text-sm text-gray-400">{m['pages.upload.uploaded.description']()}</p>
 						</div>
@@ -70,9 +70,9 @@
 							<button type="button" class="btn btn-success" onclick={closePopup}>{m['actions.okay']()}</button>
 						</div>
 					{:else}
-						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploading.title']()}</h2>
+						<h3 id="dialog-title" class="mb-6">{m['pages.upload.uploading.title']()}</h3>
 						<div class="mt-2">
-							<p class="text-sm text-gray-400">{m['pages.upload.uploading.description']()}</p>
+							<p class="text-sm text-gray-400">{m['pages.upload.uploading.description']({ progress: String(progress) })}</p>
 						</div>
 						<div class="flex items-center space-x-4 justify-center">
 							<button type="button" class="btn btn-danger" onclick={cancelUpload}>{m['actions.cancel']()}</button>
